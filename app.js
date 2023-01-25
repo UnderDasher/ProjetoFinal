5b79ac29626fb22947aa7752c1e8f3f0b78a649a
UnderDasher <122793872+UnderDasher@users.noreply.github.com>
2023-01-25 12:51:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+
+const router = express.Router();
+router.get('/', (req, res) => { 
+    res.send("ola mundo");
+
+});
+
+
+const app = express();
+app.use('/', router);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+
+const app = require('./app');
+const mongoose = require('mongoose');
+
+
+mongoose.set('strictQuery' , false);
+
+require('dotenv').config({path:'variables.env'});
+
+
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error' , (error) => {
+     console.log("ERROR: " + error.message);
+
+})
+
+app.set('port',process.env.PORT);
+const server = app.listen(app.get('port'),() =>{
+    console.log("SERVIDOR RODANDO NA PORTA: " + server.address().port);
+});
+ 
