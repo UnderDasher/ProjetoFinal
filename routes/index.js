edce853e07915d7b14293a1a63f14c1c1a0403de
UndersonDasher <undersondasher@gmail.com>
2023-01-26 12:36:14 -0300

    express

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/posts/:slug', (req, res) => { 
-    let slug = req.params.slug;
-    res.send("slug   do post: "+ slug)    ;
-});
-
-router.get('/rota1', (req, res) => { 
-    res.send("rodando do index rota1");
-});
-
-
-router.get('/rota2', (req, res) => { 
-    let nome = req.query.nome;
-    let sobrenome = req.query.sobrenome;
-    let idade = req.query.idade;
-    res.json({
-        dados: nome + ' ' + sobrenome + ' tem ' + idade
-    })
+router.get('/', (req, res) => { 
+    let obj = {
+        'nome': req.query.nome,
+    };
+    res.render('home',obj);
 });
 
 module.exports = router;
---
+++ routes/index_old.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/posts/:slug', (req, res) => { 
+    let slug = req.params.slug;
+    res.send("slug   do post: "+ slug)    ;
+});
+
+router.get('/rota1', (req, res) => { 
+    res.send("rodando do index rota1");
+});
+
+
+router.get('/rota2', (req, res) => { 
+    res.json(req.query);
+});
+
+module.exports = router;
