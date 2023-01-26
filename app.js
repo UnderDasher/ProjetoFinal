4ec259c89596ffbe73100d3383912bcffe9995ee
Aluno <Aluno@fatecdiadema.com>
2023-01-26 08:12:56 -0300

    express v2

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-
 const express = require("express");
-
-const router = express.Router();
-router.get('/', (req, res) => { 
-    res.send("ola mundo");
-
-});
-
 
 const app = express();
 app.use('/', router);
