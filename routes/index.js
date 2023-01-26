f72da07f53bb2c58928678773c8222d97affa39d
UndersonDasher <Aluno@fatecdiadema.com>
2023-01-26 08:54:35 -0300

    express

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/', (req, res) => { 
     res.send("rodando do index");
+});
+
+router.get('/rota1', (req, res) => { 
+    res.send("rodando do index rota1");
+});
+
+router.get('/rota2', (req, res) => { 
+    let nome = req.query.nome;
+    let idade = req.query.idade;
+    res.send("Ola "+ nome + " voce tem "+ idade + " anos de idade");
 });
 
 module.exports = router;
